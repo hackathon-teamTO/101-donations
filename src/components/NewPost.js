fe5a4d68fc690e241517e5a0612a5c7b0ec83c34
Chris <aqui.christine@gmail.com>
2018-09-22 02:13:31 -0400

    changed some class to classnames and working on making charities selectable

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -4,12 +4,16 @@ import "bootstrap-input-spinner";
 const styles = {
   iconsBox: {
     height: '150px',
-    overflow: "scroll"
+    display: 'flex',
+    'flex-wrap': "wrap",
+    overflow: "scroll",
+    'justify-content': 'center'
   },
   iconsLayout: {
     padding: '10px',
     display: 'flex',
-    'flex-direction': 'row'
+    'flex-direction': 'column',
+
   }
 
 }
@@ -19,10 +23,10 @@ class NewPost extends Component {
     return (
       <div style={{ marginTop: '80px',  marginLeft: '50px', marginRight: '50px' }}>
         <form className='col-md-10'>
-        <div class="form-group row">
-          <div class="form-group col-md-8">
+        <div className="form-group row">
+          <div className="form-group col-md-8">
             <label htmlFor="donation-selector">Donation Categories.</label>
-            <select class="form-control" id="donation-selector">
+            <select className="form-control" id="donation-selector">
               <option>Apparel</option>
               <option>Electronics</option>
               <option>Non Perishable</option>
@@ -30,38 +34,38 @@ class NewPost extends Component {
             </select>
           </div>
         </div>
-        <div class="form-group row">
-          <div class="form-group col-md-8">
+        <div className="form-group row">
+          <div className="form-group col-md-8">
           <label htmlFor="dQuantity">Quantity</label>
           <input  className='form-control col-md-8' id='dQuantity' type="number" placeholder="Enter a Quantity"
           />
           </div>
         </div>
 
-        <div class="form-group row">
-          <div class="form-group col-md-8">
-            <div className="" style={styles.iconsBox}>
+        <div className="form-group row">
+          <div className="form-group col-md-8">
+            <div  multiple className="form-control"style={styles.iconsBox}>
 
-              <div style={styles.iconsLayout}>
-                <a href="https://placeholder.com">
+              <div class="form-group" style={styles.iconsLayout}>
+                <a href="https://www.furniturebank.org/" target="_blank">
                 <img id='fb' src="https://via.placeholder.com/100x100"></img></a>
                 <label htmlFor='fb'>Furniture Bank</label>
               </div>
 
-              <div style={styles.iconsLayout}>
-                <a href="https://placeholder.com">
+              <div class="form-group" style={styles.iconsLayout}>
+                <a href="www.childrensbookbank.com" target="_blank">
                 <img id='fb' src="https://via.placeholder.com/100x100"></img></a>
                 <label htmlFor='fb'>Book bank</label>
               </div>
 
-              <div style={styles.iconsLayout}>
-                <a href="https://placeholder.com">
+              <div class="form-group" style={styles.iconsLayout}>
+                <a href="https://thelocalcharity.com" target="_blank">
                 <img id='fb' src="https://via.placeholder.com/100x100"></img></a>
                 <label htmlFor='fb'>Local Charity</label>
               </div>
 
-              <div style={styles.iconsLayout}>
-                <a href="https://placeholder.com">
+              <div class="form-group" style={styles.iconsLayout}>
+                <a href="www.torontoyouth.org/" target="_blank">
                 <img id='fb' src="https://via.placeholder.com/100x100"></img></a>
                 <label htmlFor='fb'>Toronto Youth</label>
               </div>
@@ -70,17 +74,17 @@ class NewPost extends Component {
           </div>
         </div>
 
-        <div class="form-group row">
-          <div class="form-group col-md-8">
+        <div className="form-group row">
+          <div className="form-group col-md-8">
           </div>
         </div>
 
-        <div class="form-group row">
-          <div class="form-group col-md-8">
+        <div className="form-group row">
+          <div className="form-group col-md-8">
           </div>
         </div>
 
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" className="btn btn-primary">Submit</button>
         </form>
       </div>
     );
