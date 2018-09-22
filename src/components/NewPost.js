680bdc361d23faa2cf22f31dc77d80c9f8c6bc05
Chris <aqui.christine@gmail.com>
2018-09-22 10:56:55 -0400

    form inputs added now to connect to state

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -6,14 +6,13 @@ const styles = {
     height: '150px',
     display: 'flex',
     'flex-wrap': "wrap",
-    overflow: "scroll",
+    'overflow': "scroll",
     'justify-content': 'center'
   },
   iconsLayout: {
     padding: '10px',
     display: 'flex',
     'flex-direction': 'column',
-
   }
 
 }
@@ -37,51 +36,67 @@ class NewPost extends Component {
         <div className="form-group row">
           <div className="form-group col-md-8">
           <label htmlFor="dQuantity">Quantity</label>
-          <input  className='form-control col-md-8' id='dQuantity' type="number" placeholder="Enter a Quantity"
-          />
+          <input  className='form-control col-md-8' id='dQuantity' type="number" placeholder="Enter a Quantity"/>
           </div>
         </div>
 
         <div className="form-group row">
           <div className="form-group col-md-8">
           <h6>Select a charity</h6>
-            <div  multiple className="form-control"style={styles.iconsBox}>
-
-              <div class="form-group" style={styles.iconsLayout}>
-                <a href="https://www.furniturebank.org/" target="_blank">
-                <img id='fb' src="https://via.placeholder.com/100x100"></img></a>
+            <div  className="form-control"style={styles.iconsBox}>
+              <div className="form-group" style={styles.iconsLayout}>
+                <img id='fb' src="https://via.placeholder.com/100x100"></img>
                 <label htmlFor='fb'>Furniture Bank</label>
               </div>
-
-              <div class="form-group" style={styles.iconsLayout}>
-                <a href="www.childrensbookbank.com" target="_blank">
-                <img id='fb' src="https://via.placeholder.com/100x100"></img></a>
-                <label htmlFor='fb'>Book bank</label>
+              <div className="form-group " style={styles.iconsLayout}>
+                <img id='cbd' src="https://via.placeholder.com/100x100"></img>
+                <label htmlFor='cbd'>Book bank</label>
               </div>
-
-              <div class="form-group" style={styles.iconsLayout}>
-                <a href="https://thelocalcharity.com" target="_blank">
-                <img id='fb' src="https://via.placeholder.com/100x100"></img></a>
-                <label htmlFor='fb'>Local Charity</label>
+              <div className="form-group " style={styles.iconsLayout}>
+                <img id='lc' src="https://via.placeholder.com/100x100"></img>
+                <label htmlFor='lc'>Local Charity</label>
               </div>
-
-              <div class="form-group" style={styles.iconsLayout}>
-                <a href="www.torontoyouth.org/" target="_blank">
-                <img id='fb' src="https://via.placeholder.com/100x100"></img></a>
-                <label htmlFor='fb'>Toronto Youth</label>
+              <div className="form-group " style={styles.iconsLayout}>
+                <img id='tyd' src="https://via.placeholder.com/100x100"></img>
+                <label htmlFor='tyd'>Toronto Youth</label>
               </div>
+            </div>
+            <div className="form-check form-check-inline">
+              <input className="form-check-input" type="checkbox" id="inlineCheckbox2" value="all"></input>
+              <label className="form-check-label" htmlFor="inlineCheckbox2">Select All</label>
+            </div>
 
+          </div>
+        </div>
+
+
+        <div className="form-group row">
+          <div className="form-group col-md-8">
+            <div className="form-group">
+              <label htmlFor="exampleFormControlFile1">Picture Image</label>
+              <input type="file" className="form-control-file" id="exampleFormControlFile1"></input>
             </div>
           </div>
         </div>
 
         <div className="form-group row">
+        <hr/>
           <div className="form-group col-md-8">
-          </div>
-        </div>
+          <h6>Dornator inforamtion:</h6>
 
-        <div className="form-group row">
-          <div className="form-group col-md-8">
+              <div className="form-group col">
+                <input type="text" className="form-control" placeholder="First name"></input>
+              </div>
+              <div className="form-group col">
+                <input type="text" className="form-control" placeholder="Last name"></input>
+              </div>
+              <div class="form-group col">
+                <input type="text" className="form-control" id="inputAddress" placeholder="Address"></input>
+              </div>
+              <div className="form-group col">
+                <input type="text" className="form-control" id="inputZip" placeholder="Zip Code"></input>
+              </div>
+
           </div>
         </div>
 
