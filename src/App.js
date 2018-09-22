a3c6bbd43bf83e19f72e2c1981dc5e9b56ed0e3a
yung dev <andre.torquati@gmail.com>
2018-09-22 17:33:27 -0400

    did few last changes

--- src/App.js
+++ src/App.js
@@ -6,11 +6,10 @@ import './App.css';
 import Header from './components/Header';
 import CharityPostFeed from './components/CharityPostFeed';
 import Login from './components/Login';
-// import Donator from './components/Donator';
+import Donator from './components/Donator';
 import NewPost from './components/NewPost';
 import Landing from './components/Landing';
 import UserDashboard from "./components/UserDashboard";
-import BarChart from './components/BarChart';
 
 const config = {
   apiKey: "AIzaSyB4pcLbkq9H1psYfWY0kF9W9F1raOSErH0",
@@ -23,12 +22,7 @@ const config = {
 firebase.initializeApp(config);
 
 class App extends Component {
- state = {
-  data: [12, 5, 6, 6, 9, 10],
-  width: 700,
-  height: 500,
-  id: root
- }
+ 
   render() {
     return (
       <div>
